85afa5c8a2bce76db5e1138dad6feca8e46371ac
Sing Tuttanon <singtuttanon@gmail.com>
2023-10-10 15:14:58 -0400

    Theater Reservation Completed

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,31 +1,35 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class TheaterReservations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//  Prompt user for their full name
+//  Prompt the user for their full name
         System.out.print("Please enter your full name (First Middle Last): ");
         String fullName = scanner.nextLine();
 
-//  Prompt user for the date of the show
-        System.out.print("What date will you be attending (MM/dd/yyyy): ");
-        String showDate = scanner.nextLine();
+//  Prompt the user for the date of the show
+        System.out.print("What date will you be attending (yyyy-MM-dd): ");
+        String dateStr = scanner.nextLine();
+        LocalDate date = LocalDate.parse(dateStr);
 
-//  Prompt user for the number of tickets needed
+//  Prompt the user for the number of tickets
         System.out.print("How many tickets would you like? ");
         int numTickets = scanner.nextInt();
 
-//  Close the scanner
+//  Format the reservation confirmation message with MM/dd/yyyy date format
+        String formattedDate = date.format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+        String[] nameParts = fullName.split(" ");
+        String lastName = nameParts[nameParts.length - 1];
+        String firstName = nameParts.length > 1 ? nameParts[0] : "";
+
+        System.out.print(numTickets + " ticket" + (numTickets > 1 ? "s" : "") + " reserved for " + formattedDate +
+                " under " + lastName + ", " + firstName);
+
         scanner.close();
-
-//  Prepare the confirmation message
-        String pluralSuffix = numTickets == 1 ? "" : "s";
-        String confirmationMessage = numTickets + " ticket" + pluralSuffix + " reserved for " + showDate + " under " + fullName;
-
-//  Display the confirmation message
-        System.out.println(confirmationMessage);
     }
 }
